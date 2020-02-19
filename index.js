4524f0df7115f611608ec6537137c11241b913a7
Alexander Behrmann <alexander.behrmann@hotmail.com>
2020-02-19 12:23:55 +0000

    Updating cors header

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'https://master.d208h0gxtroz1d.amplifyapp.com');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Credentials', true);
     next();
